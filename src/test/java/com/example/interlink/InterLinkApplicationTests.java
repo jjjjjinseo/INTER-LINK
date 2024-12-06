086b14cf0cde37fb8cf89953a758068e26c6ccfc
wlstj <22020306jinseoyeon@gmail.com>
2024-12-06 12:53:03 +0900

    build: initial setting

---
+++ src/test/java/com/example/interlink/InterLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.interlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterLinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
