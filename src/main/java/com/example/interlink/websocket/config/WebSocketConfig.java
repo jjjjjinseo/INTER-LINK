3b587f6b75ada4be19a2ee68500c94f8677d8c87
wlstj <22020306jinseoyeon@gmail.com>
2024-12-12 18:36:45 +0900

    feat: 웹소켓 cors 주소 추가

--- src/main/java/com/example/interlink/websocket/config/WebSocketConfig.java
+++ src/main/java/com/example/interlink/websocket/config/WebSocketConfig.java
@@ -19,6 +19,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins("http://localhost:8080", "http://127.0.0.1:8080",
+                        "http://43.201.250.37:8080", "http://3.35.210.245:8080",
+                        "http://metal-load-balancer-300049590.ap-northeast-2.elb.amazonaws.com:8080")
+                .withSockJS();
     }
 }
